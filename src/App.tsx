ebb98080368be95f8b26eb6e3bba27cf53a1204c
EternalWill43 <70084418+EternalWill43@users.noreply.github.com>
2022-03-19 23:37:53 -0400

    addcomp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestShit from './components/TestShit';
 
 function App() {
   return (
     <div className="App">
-      
+      <TestShit/>
     </div>
   );
 }
---
+++ src/components/TestShit.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestShit() {
+  return (
+    <View>
+      <Text>TestShit</Text>
+    </View>
+  )
+}
